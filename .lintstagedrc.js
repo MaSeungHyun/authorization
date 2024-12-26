412b86d7816ac11036d43fe4785597f6de810b29
MaSeungHyun <seunghyun.ma@grapicar.com>
2024-12-26 15:28:30 +0900

    setting: 컨벤션 관련 개발환경 셋팅 husky, lint-staged eslint, prettier

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+// .lintstagedrc.js
+import path from 'path';
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+    
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+};
